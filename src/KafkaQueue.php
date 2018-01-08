ffee1083cc071c7436bfadd2f67b881729e6921e
ellis <1019382852@qq.com>
2018-01-08 16:47:04 +0800

    Update KafkaQueue.php

--- src/KafkaQueue.php
+++ src/KafkaQueue.php
@@ -164,7 +164,9 @@ class KafkaQueue extends Queue
     public function later($delay, $job, $data = '', $queue = null)
     {
         // The Kafka does not support later
-        throw new \Exception('kafka not support delay until now');
+        //throw new \Exception('kafka not support delay until now');
+        $this->push($job, $data, $queue);
+        
     }
 
     /**
